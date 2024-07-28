685fc6105363cf5fd222201a04438bb5587be70c
TennoAntenno <jedidja873@gmail.com>
2024-07-28 16:13:31 +0300

    Add ability to hold down control to delete entire words

--- Program.cs
+++ Program.cs
@@ -1,5 +1,24 @@
 ﻿class Program
 {
+    public static string DeleteChar(string text)
+    {
+        return text.Substring(0, text.Length - 1);
+    }
+    public static string DeleteWord(string text)
+    {
+        for (int i = text.Length - 1; i >= 0; i--)
+        {
+            if (!(text[i] > 'A' && text[i] < 'z'))
+            {
+                if (i == text.Length -1 )
+                    text = text.Substring(0, i);
+                break;
+            }
+            text = text.Substring(0, i);
+        }
+        return text;
+    }
+
     public static void Main(string[] args)
     {
         string text = "";
@@ -14,8 +33,10 @@
             }
             else if (key.Key == ConsoleKey.Backspace)
             {
-                if (text.Length > 0)
-                    text = text.Substring(0, text.Length - 1);
+                if (key.Modifiers == ConsoleModifiers.Control)
+                    text = DeleteWord(text);
+                else if (text.Length > 0)
+                    text = DeleteChar(text);
             }
             else if (key.Key == ConsoleKey.Escape)
             {
--- obj/Debug/net8.0/cli-text-editor.AssemblyInfo.cs
+++ obj/Debug/net8.0/cli-text-editor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cli-text-editor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d748607660c613d62debf894d6b9193c6cec66f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56dfb376c06cc58aadd3e187c84ad8692d5c6806")]
 [assembly: System.Reflection.AssemblyProductAttribute("cli-text-editor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cli-text-editor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
