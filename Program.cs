56dfb376c06cc58aadd3e187c84ad8692d5c6806
TennoAntenno <jedidja873@gmail.com>
2024-07-28 15:23:16 +0300

    Add ability to write text

--- Program.cs
+++ Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        string text = "";
+        while (true)
+        {
+            Console.Clear();
+            Console.Write(text);
+            ConsoleKeyInfo key = Console.ReadKey(true);
+            if (key.Key == ConsoleKey.Enter)
+            {
+                text += '\n';
+            }
+            else if (key.Key == ConsoleKey.Backspace)
+            {
+                if (text.Length > 0)
+                    text = text.Substring(0, text.Length - 1);
+            }
+            else if (key.Key == ConsoleKey.Escape)
+            {
+                break;
+            }
+            else
+            {
+                text += key.KeyChar;
+            }
+        }
+    }
+}
